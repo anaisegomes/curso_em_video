c39d33e24544fa3e16278a9a8a4cbdbaab7d2307
Anaíse Gomes <110070867+anaisegomes@users.noreply.github.com>
2025-03-14 09:54:04 -0300

    Create aula06.py

---
+++ aula06.py
@@ -0,0 +1,11 @@
+n1 = int(input('Digite um numero: '))
+n2 = int(input('Digite mais um numero: '))
+soma = n1 + n2
+# print('a soma vale', soma)
+print('a soma vale {}'.format(soma))
+
+
+# int - numeros interos ///exemplos:  7 / -4 // 0  // 9875
+# float - numeros reais (ou numeros com virgulas) exemp: 4.5 // 0.076 // -15.223 // 7.0
+# # bool  - valores logicos (ou booleano)  // True // False
+# str - valores caracteres ou strings  /// 'Ola' '7.5' '' (string vazia)
