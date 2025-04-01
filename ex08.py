77b1cdbc0739967db4b7c6fd0ca5e5d03579861b
Anaíse Gomes <110070867+anaisegomes@users.noreply.github.com>
2025-04-01 06:38:44 -0300

    Update ex08.py

--- ex08.py
+++ ex08.py
@@ -1,5 +1,10 @@
 medida = float(input('Uma distancia em metros: '))
 
+dm = medida * 10
 cm = medida * 100
 mm = medida * 1000
-print('A medida de {}m corresponde a {}cm e {}mm'.format(medida, cm, mm))
+dam = medida / 10
+hm = medida / 100
+km = medida /1000
+
+print('A medida de {}m corresponde a \n{}cm e \n{}mm, \n{}dm \n{}dam \n{}hm \n{}km'.format(medida, cm, mm, dm, dam, hm, km))
